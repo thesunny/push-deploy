41ac7227cb1a951fbb6a9b215dd7748cc603419d
Sunny Hirai <thesunny@gmail.com>
2022-02-08 19:58:30 -0800

    Argument check ARGV

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import { predeploy } from "./predeploy"
-import { deploy } from "./deploy"
+import { predeploy } from "./predeploy";
+import { deploy } from "./deploy";
 
-const [_0, _1, PRIMARY_BRANCH, DEPLOY_BRANCH] = process.argv
+const [_0, _1, PRIMARY_BRANCH, DEPLOY_BRANCH] = process.argv;
 
-predeploy(PRIMARY_BRANCH, DEPLOY_BRANCH)
-deploy(PRIMARY_BRANCH, DEPLOY_BRANCH)
+if (typeof PRIMARY_BRANCH !== "string") {
+  throw new Error(`Expected first argument for PRIMARY_BRANCH to be defined`);
+}
+
+if (typeof DEPLOY_BRANCH !== "string") {
+  throw new Error(`Expected second argument for DEPLOY_BRANCH to be defined`);
+}
+
+predeploy(PRIMARY_BRANCH, DEPLOY_BRANCH);
+deploy(PRIMARY_BRANCH, DEPLOY_BRANCH);
